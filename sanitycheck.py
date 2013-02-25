b01adcac2b5cf45070b56ec170549fd9be3a53c8
Franklin Nwankwo <franklin.nwankwo@gmail.com>
2013-02-25 00:57:26 +0000

    added properties check

--- sanitycheck.py
+++ sanitycheck.py
@@ -9,6 +9,7 @@ def isbool(value):
 		return bool(1)
 	else: 
 		return bool(0)
+CONFIG_FILE='c:\dev\sanitycheck.config'
 
 HEADER_DATE_IDX=0
 HEADER_REC_IDX=1		
@@ -18,6 +19,7 @@ MAX_FOOTER_LEN = 1;
 SYMBOL_DATE_FORMAT='%d/%m/%Y'
 TICKER_SYMBOL_IDX = 1
 SYM_SHORT_SELL_IDX = 2
+SYMBOL_COL_IDX = 0
 
 #map of symbol column header index to a tuple of its label and data type (for validation)
 symbolHeaderMap={TICKER_SYMBOL_IDX : ('TICKER SYMBOL', str), SYM_SHORT_SELL_IDX : ('SHORT SELL RESTRICTED', isbool), 3 :  ('MIN TICK SIZE', float), 4 : ('MAX PRICE DEVIATION', int), 5 : ('PREVIOUS CLOSE PRICE', float) , 6 : ('ACTIVE', bool)}
@@ -25,7 +27,7 @@ symbolHeaderMap={TICKER_SYMBOL_IDX : ('TICKER SYMBOL', str), SYM_SHORT_SELL_IDX 
 COL_HDR_IDX = 1 #index of column header in tuple
 
 #map of client limits column header index to a tuple of its equivalent in original/source client file, label and type (for validation)
-clientLimitsHeaderMap={1:(1,'SESSION ID',str), 3:(2, 'CLIENT ID', str),4:(3,'MAX ORDER SHARES',int),5:(4,'MAX ORDER VALUE',float),6:(5,'DAILY SESSION CONSIDERATION',float),7:(6,'OVERALL CLIENT CONSIDERATION',float),8:(7,'SHORT SELL CHECK',isbool),28:(8,'THRESHOLD',int),29:(9,'THRESHOLDINC',int), 9:(10,'ACTIVE',isbool)}
+clientLimitsHeaderMap={1:(1,'SESSION ID',str), 2:(3, 'CLIENT ID', str),3:(4,'MAX ORDER SHARES',int),4:(5,'MAX ORDER VALUE',float),5:(6,'DAILY SESSION CONSIDERATION',float),6:(7,'OVERALL CLIENT CONSIDERATION',float),7:(8,'SHORT SELL CHECK',isbool),8:(28,'THRESHOLD',int),9:(29,'THRESHOLDINC',int), 10:(9,'ACTIVE',isbool)}
 
 #map of client session config column header index to a tuple of its equivalent in original/source client file, label and type (for validation)
 sessionConfigHeaderMap={1: ( 1, 'CLIENT SESSION ID', str), 2: ( 2, 'VENUE SESSION ID',str), 3: ( 10,'PROTOCOL VERSION', str), 4: (11, 'VENUE REMOTE IP ADDRESS', str), 
@@ -34,6 +36,17 @@ sessionConfigHeaderMap={1: ( 1, 'CLIENT SESSION ID', str), 2: ( 2, 'VENUE SESSIO
 	15: (14, 'CLIENT SENDERCOMID',str), 16: (18, 'VENUE TARGETCOMPID',str), 17: (25,'CLIENT TARGETCOMPID', str), 18: (19, 'VENUE SENDERSUBID',str), 19: (26, 'CLIENT SENDERSUBID', str), 
 	20: (20,'VENUE TARGETSUBID',str), 21: (27, 'CLIENT TARGETSUBID',str), 22: (30, 'CLIENT GATEWAY IP','str'), 23: (31,'VENUE GATEWAY IP', str), 24: (9,'ACTIVE',  str)}
 
+
+def loadConfigFile():
+        f = open(CONFIG_FILE)
+        configs = f.readlines()
+        configmap={}
+        for row in configs:
+                keyval=row.split('=')
+                configmap[keyval[0]] = keyval[1]
+        return configmap
+        
+        
 def fetchRawInputData():
 	pass
 
@@ -98,12 +111,30 @@ def isDateValid(recdate):
 		
 
 def rawSymbolDataToIxEye(symbolData):
-	for x,token in enumerate(symbolData):
+        emailContentBuf = []
+	validateSourceHdrFtr(symbolData, emailContentBuf)
+
+	ixEyeData = [] #target data list 
+	#build column header 
+	ixEyeData.append(','.join(col[SYMBOL_COL_IDX] for col in symbolHeaderMap.values()))
+        #take copy of client records (omit header and footer)
+	clientRecords = symbolData[1:len(symbolData) - 1]
+	ixEyeData.append(''.join(clientRecords))
+
+        #saveToIExEyeFile(ixEyeData, os.sys.argv[2])
+	f=open(os.sys.argv[2], 'w')
+        if not f:
+                raise Exception('No such file {0}'.format(os.sys.argv[2]))
+        for item in ixEyeData:
+          print>>f, item
+	pass
+	"""
+	for x,token in enumerate(symbolData):s
 		mapval = symbolHeaderMap[x+1]		
 		validator = mapval[1]	
 		print validator(token)	
 		#print token		
-
+        """
 
 def rawClientConfigToIxEye(clientConfigData):
 	emailContentBuf = []
@@ -113,24 +144,22 @@ def rawClientConfigToIxEye(clientConfigData):
 	ixEyeData = [] #target data list 
 	#build column header 
 	ixEyeData.append(','.join(col[COL_HDR_IDX] for col in clientLimitsHeaderMap.values()))
-
+        
         #using data in predefined map, map each client record to ixEye equivalent
         for clientRow in clientRecords:
-                clientFields = clientRow.split(',')
+                clientFields = clientRow.split(',')               
                 ixEyeRow = []
                 for colIdx in clientLimitsHeaderMap.keys():
                         colTuple = clientLimitsHeaderMap[colIdx]
-                        ixEyeRecord.append(clientFields[colTuple[0]])
-                ixEyeData.append(','.join(ixEyeRecord))
-                
-	for colIdx in clientLimitsHeaderMap.keys():
-                rec = records[colIdx]
-                if len(rec) == len(clientLimitsHeaderMap):
-                        tokens = rec.split(',');
-                        for i,tok in enumerate(tokens):
-                                client
-                        
-        pass
+                        ixEyeRow.append(clientFields[colTuple[0] - 1]) # get equivalent value from source data  
+                ixEyeData.append(','.join(ixEyeRow))
+
+        f=open(os.sys.argv[2], 'w')
+        if not f:
+                raise Exception('No such file {0}'.format(os.sys.argv[2]))
+        for item in ixEyeData:
+          print>>f, item
+	pass
 	
 def sendmail(content):
 	pass
@@ -146,9 +175,13 @@ if not f:
 	
 lines = f.readlines()
 
-parseSymbolRawInput(lines)
+#parseSymbolRawInput(lines)
 
-rawClientConfigToIxEye(lines)
+#rawClientConfigToIxEye(lines)
+
+print loadConfigFile()
+
+rawSymbolDataToIxEye(lines)
 
 """
 test.txt
